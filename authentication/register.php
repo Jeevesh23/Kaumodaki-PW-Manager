985126ce6d1381400aff0e04e1792538ec76d778
Mahendra Priolkar <mahepriol1313@gmail.com>
2023-08-11 23:52:36 +0530

    login page (auth module)

---
+++ authentication/register.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '','PM_1');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+require_once('func.php');
+$salt = getRandomStringRand();
+$hasheddata = hash('sha512', $password.$salt);
+
+$sql = "INSERT INTO `Credentials` (`Username`, `Password`, `Salt`) VALUES ('$username','$hasheddata','$salt')";
+$result = mysqli_query($conn, $sql);
+
+if(!$result)
+    echo "Connection failed";
+else
+    echo "User registered!";
+
+$conn->close();
+
+?>
---
+++ authentication/signin.php
@@ -0,0 +1,31 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '','PM_1');
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$sql = "SELECT `Salt`,`Password` FROM `Credentials` WHERE `Username`='$username'";
+$result = mysqli_query($conn, $sql);
+
+if(!$result)
+    echo "Connection failed";
+else if($result->num_rows > 0)
+{
+    while($row = $result->fetch_assoc())
+    {
+        $hasheddata = hash('sha512',$password.$row["Salt"]);
+        if($row["Password"]==$hasheddata)
+            echo "User signed in!";
+    }
+}
+else
+{
+    echo "No user!Register now";
+}
+
+$conn->close();
+
+?>
