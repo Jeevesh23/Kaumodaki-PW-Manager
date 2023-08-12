6adc98b782e0c4077858aa8f659f438c87c86900
Mahendra Priolkar <mahepriol1313@gmail.com>
2023-08-12 07:25:18 +0530

    cleaning up code

--- authentication/func.php
+++ authentication/func.php
@@ -4,7 +4,7 @@ function getRandomStringRand($length = 32)
     $stringSpace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $stringLength = strlen($stringSpace);
     $randomString = '';
-    for ($i = 0; $i < $length; $i ++) {
+    for ($i = 0; $i < $length; $i++) {
         $randomString = $randomString . $stringSpace[rand(0, $stringLength - 1)];
     }
     return $randomString;
--- authentication/signin.php
+++ authentication/signin.php
@@ -1,8 +1,8 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '','PM_1');
+$conn = mysqli_connect('localhost', 'root', '', 'PM_1');
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $username = $_POST['username'];
@@ -10,19 +10,15 @@ $password = $_POST['password'];
 $sql = "SELECT `Salt`,`Password` FROM `Credentials` WHERE `Username`='$username'";
 $result = mysqli_query($conn, $sql);
 
-if(!$result)
+if (!$result)
     echo "Connection failed";
-else if($result->num_rows > 0)
-{
-    while($row = $result->fetch_assoc())
-    {
-        $hasheddata = hash('sha512',$password.$row["Salt"]);
-        if($row["Password"]==$hasheddata)
+else if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $hasheddata = hash('sha512', $password . $row["Salt"]);
+        if ($row["Password"] == $hasheddata)
             echo "User signed in!";
     }
-}
-else
-{
+} else {
     echo "No user!Register now";
 }
 
