3e64192d2c660c5d07eaf972a6e7b0dbb112b608
Jeevesh23 <114322101+Jeevesh23@users.noreply.github.com>
2023-08-26 16:32:14 +0530

    made the qrcode page more beautiful

--- authentication/register-form.php
+++ authentication/register-form.php
@@ -23,21 +23,88 @@ $_SESSION['secret'] = $secret;
 
 <head>
     <title>Two-Factor Authentication</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: 'Poppins', sans-serif;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-image: linear-gradient(142deg, #9861c2, #5cc0de);
+            font-family: "Lato", sans-serif;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            overflow: hidden;
+            margin: 0;
+        }
+
+        p {
+            background-color: rgba(255,255,255.0.3);
+            font-size: 20px;
+            border-radius: 10px;
+            max-width: 1000px; 
+            text-align: center;
+        }
+
+        .qr-code {
+            max-width: 1100px;
+            width: 30%;
+            margin: 0 auto;
+            text-align: center;
+            padding: 25px 35px;
+            background: #f5f5f5;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+        }
+
+        .Form .btn {
+            background: #231919;
+            color: white;
+            margin-top: 30px;
+            width: 100%;
+            padding: 10px 0;
+            border-radius: 10px;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="Code">
-        <p>Scan the following image with your authenticator app:</p>
-        <img src="<?php
-        echo $tfa->getQRCodeImageAsDataUri($_SESSION['username'] . '_' . $_SESSION['email'], $secret, 400); ?>">
-    </div>
-    <div class="Form">
+    <div class="qr-code">
+        <br><br><p>Scan the following image with your authenticator app:</p><br><br>
+        <div class="qr-box">
+            <img src="<?php
+            echo $tfa->getQRCodeImageAsDataUri($_SESSION['username'] . '_' . $_SESSION['email'], $secret, 400); ?>">
+        </div>
+        
+        <div class="Form">
         <form method="post" action="./register-otp.php">
-            <label for="otp">Enter OTP: </label>
-            <input type="text" placeholder="Your OTP" name="otp" id="otp" required>
-            <button type="submit">Submit</button>
+            <label for="otp"><br>Enter OTP: </label>
+            <input type="text" placeholder=" Your OTP" name="otp" id="otp" required><br>
+            <button type="submit" class="btn">Submit</button>
         </form>
     </div>
-</body>
+    </div>
+    
+<!--My code which was not working-->
+    <!-- <div class="qr-code">
+        <div class="container">
+            <h2>Scan the following image with your authenticator app</h2><br><br>
+        </div>
 
+        <img src="<?php
+        echo $tfa->getQRCodeImageAsDataUri($_SESSION['username'] . '_' . $_SESSION['email'], $secret, 400); ?>">
+
+        <div class="Form">
+            <form method="post" action="./register-otp.php">
+                <label for="otp"><br>Enter OTP: </label>
+                <input type="text" placeholder=" Your OTP" name="otp" id="otp" required><br><br>
+                <button type="button" class="btn">Submit</button>
+            </form>
+        </div>
+    </div> -->
+</body>
 </html>
