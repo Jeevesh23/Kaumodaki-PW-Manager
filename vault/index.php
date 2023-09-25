17d473391eeeff81fe852fb66e893869d2b4b9ea
Jeevesh23 <114322101+Jeevesh23@users.noreply.github.com>
2023-09-25 17:14:45 +0530

    added vault page

--- vault/index.php
+++ vault/index.php
@@ -1,140 +1,152 @@
 <?php
-require_once(__DIR__ . '/config/db.php');
-$query = "SELECT * FROM User_Info";
-$result = mysqli_query($con, $query);
+require_once('/vault/config/db.php');
+$query = "select * from User_Info";
+$result = mysqli_query($con,$query);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="/vault/style.css">
     <title>Vault</title>
 </head>
 
 <body>
-    <div class="sidebar">
-        <div class="logo_content">
-            <div class="logo">
-                <i class='bx bxl-netlify'></i>
-                <div class="logo_name">Password Manager</div>
+    <div class="container">
+        <!-- Sidebar Section -->
+        <aside>
+            <div class="toggle">
+                <div class="logo">
+                    <!-- <img src="images/profile.jpg"> -->
+                    <!-- <i class='bx bxl-netlify'></i> -->
+                    <h2>Password<br><span class="danger">Manager</span></h2>
+                </div>
+                <div class="close" id="close-btn">
+                    <span class="material-icons-sharp">
+                        close
+                    </span>
+                </div>
             </div>
-            <i class="bx bx-menu" id="btn"></i>
-            <ul class="nav_list">
-                <li>
-                    <i class='bx bx-search'></i>
-                    <input type="text" placeholder="Search...">
-                    <span class="tooltip">Search</span>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class='bx bx-grid-alt'></i>
-                        <span class="links_name">Dashboard</span>
-                    </a>
-                    <span class="tooltip">Dashboard</span>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class='bx bx-user'></i>
-                        <span class="links_name">User</span>
-                    </a>
-                    <span class="tooltip">User</span>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class='bx bx-chat'></i>
-                        <span class="links_name">Messages</span>
-                    </a>
-                    <span class="tooltip">Messages</span>
-                </li>
-                <!-- <li>
-                <a href="#">
-                    <i class='bx bx-folder'></i>
-                    <span class="links_name">File Manager</span>
+
+            <div class="sidebar">
+                <a href="index.html" class="active">
+                    <span class="material-icons-sharp">
+                        dashboard
+                    </span>
+                    <h3>Dashboard</h3>
                 </a>
-                <span class="tooltip">Files</span>
-            </li> -->
-                <li>
-                    <a href="#">
-                        <i class='bx bx-heart'></i>
-                        <span class="links_name">Saved</span>
-                    </a>
-                    <span class="tooltip">Saved</span>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class='bx bx-cog'></i>
-                        <span class="links_name">Settings</span>
-                    </a>
-                    <span class="tooltip">Settings</span>
-                </li>
-            </ul>
-            <div class="profile_content">
+                <a href="#">
+                    <span class="material-icons-sharp">
+                        person_outline
+                    </span>
+                    <h3>User</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">
+                        settings
+                    </span>
+                    <h3>Settings</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">
+                        add
+                    </span>
+                    <h3>Add Password</h3>
+                </a>
+                <a href="#">
+                    <span class="material-icons-sharp">
+                        logout
+                    </span>
+                    <h3>Logout</h3>
+                </a>
+            </div>
+        </aside>
+        <!-- End of Sidebar Section -->
+
+        <!-- Main Content -->
+        <main>
+            <h1>Dashboard</h1>
+            <!-- passwords -->
+            <div class="passwords">
+                <h2>Your Passwords</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Link</th>
+                            <th>User_ID</th>
+                            <th>Password</th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <?php
+                                while($row = mysqli_fetch_assoc($result)) {
+                            ?>  
+            
+                                <td><?php echo $row['Description'];?></td>
+                                <td><?php echo $row['Link'];?></td>
+                                <td><?php echo $row['Password'];?></td> 
+                                <td><a href="#" class="btn btn-primary">Edit</a></td>
+                                <td><a href="#" class="btn btn-danger">Delete</a></td>      
+            
+                            </tr>
+                            <?php
+                                } 
+                            ?>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+                <!-- <a href="#">Show All</a> -->
+            </div>
+            <!-- End of Recent Orders -->
+
+        </main>
+        <!-- End of Main Content -->
+
+        <!-- Right Section -->
+        <div class="right-section">
+            <div class="nav">
+                <button id="menu-btn">
+                    <span class="material-icons-sharp">
+                        menu
+                    </span>
+                </button>
+                <div class="dark-mode">
+                    <span class="material-icons-sharp active">
+                        light_mode
+                    </span>
+                    <span class="material-icons-sharp">
+                        dark_mode
+                    </span>
+                </div>
+
                 <div class="profile">
-                    <div class="profile_details">
-                        <img src="profile.jpg" alt="">
-                        <div class="name_job">
-                            <div class="name">Jeevesh</div>
-                            <div class="job">Designer</div>
-                        </div>
+                    <div class="info">
+                        <p>Hey, <b>Jeevesh</b></p>
                     </div>
-                    <i class="bx bx-log-out" id="log_out"></i>
+                    <div class="profile-photo">
+                        <img src="images/profile-1.jpg">
+                    </div>
+                </div>
+
+            </div>
+            <!-- End of Nav -->
+
+            <div class="user-profile">
+                <div class="logo">
+                    <!-- <img src="images/profile.jpg"> -->
+                    <h2>Password<br>Manager</h2>
                 </div>
             </div>
         </div>
-        <div class="home_content">
-            <div class="text">Dashboard</div>
-            <p> hi</p>
-            <div class="card-body">
-                <table class="table table-bordered text-center">
-                    <tr class="bg-dark text-white">
-                        <td> Description </td>
-                        <td> Link </td>
-                        <td> Password </td>
-                        <td> Edit </td>
-                        <td> Delete </td>
-                    </tr>
-                    <tr>
-                        <?php
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
 
-                            <td>
-                                <?php echo $row['Description']; ?>
-                            </td>
-                            <td>
-                                <?php echo $row['Link']; ?>
-                            </td>
-                            <td>
-                                <?php echo $row['Password']; ?>
-                            </td>
-                            <td><a href="#" class="btn btn-primary">Edit</a></td>
-                            <td><a href="#" class="btn btn-danger">Delete</a></td>
 
-                        </tr>
-                        <?php
-                        }
-                        ?>
-
-                </table>
-            </div>
-        </div>
+    </div>
+    <!-- <script src="orders.js"></script> -->
+    <script src="index.js"></script>
 </body>
-
-<script>
-    let btn = document.querySelector("#btn");
-    let sidebar = document.querySelector(".sidebar");
-    let searchBtn = document.querySelector(".bx-search");
-
-    btn.onclick = function () {
-        sidebar.classList.toggle("active");
-    }
-    searchBtn = function () {
-        sidebar.classList.toggle("active");
-    }
-        ;
-</script>
 
 </html>
